8fd999f72c0d72659c213adb8744c2f351d43cd7
Lonely-Crow <112963392+Lonely-Crow@users.noreply.github.com>
2022-09-25 19:40:53 +0300

    Add files via upload

---
+++ 1.1.cpp
@@ -0,0 +1,32 @@
+﻿#include "stdlib.h"
+#include <iostream>
+
+
+int main()
+{
+	setlocale(0, "");
+	int n, j, i, min = 10000, max = -1000000;
+	scanf_s("%d", &j);
+	int* a = NULL;
+	a = (int*)malloc(j * sizeof(int));
+
+	printf("массив :");
+	for (i = 0; i < j; i++)
+	{
+		a[i] = rand();
+		printf(" %d ", a[i]);
+	}
+	for (i = 0; i < j; i++)
+	{
+		if (a[i] < min)
+			min = a[i];
+		if (a[i] > max)
+			max = a[i];
+	}
+	n = max - min;
+	printf(" \n  Разница между максимальным и минимальным элементом массива =  %d ", n);
+	getchar();
+	getchar();
+	return 0;
+}
+
---
+++ лаба 1.2.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <iostream>
+#include "stdlib.h"
+
+int main()
+{
+	setlocale(0, "");
+	int  j, i, sum=0;
+	int m, n=0;
+	int** arr=NULL;
+	printf("Введите количество строк и  столбцов: ");
+	scanf_s("%d", &m);
+	
+	arr = (int**)malloc(m * sizeof(int*));
+	for (int i = 0; i < m; i++) {
+		arr[i] = (int*)malloc(m * sizeof(int));
+	}
+
+	printf("массив : \n");
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			arr[i][j] = rand() % 100;
+			printf(" %d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			sum = sum + arr[i][j];
+		}
+		printf("Сумма элементов в строке: %d \n ", sum);
+		sum = 0;
+	}
+	for (i = 0; i < m; i++)
+	{
+		for (j = 0; j < m; j++)
+		{
+			if (i <= j)
+				n += arr[i][j];
+		}
+	}
+	printf("Сумма элементов главной диагонали и элементов находящихся выше неё = %d", n);
+	getchar();
+	getchar();
+	return 0;
+}
